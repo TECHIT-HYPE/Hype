42834075e51cb01c4efceede978cafe30955ad15
tseop <77163842+seop-kim@users.noreply.github.com>
2024-01-17 17:34:53 +0900

    Project init
    gitignore 설정

---
+++ src/test/java/com/ll/hype/Hype/HypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.hype.Hype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
