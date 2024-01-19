5fc0d9168cbb6d5229a16c959f001596020b0b29
ChaChaMong <thflf93@gmail.com>
2024-01-19 10:52:50 +0900

    feat: social, review entitiy mapping 추가

--- src/main/java/com/ll/hype/domain/social/social/entity/Social.java
+++ src/main/java/com/ll/hype/domain/social/social/entity/Social.java
@@ -1,5 +1,36 @@
 package com.ll.hype.domain.social.social.entity;
 
-public class Social {
+import com.ll.hype.domain.member.member.entity.Member;
+import com.ll.hype.domain.social.socialcomment.entity.SocialComment;
+import com.ll.hype.global.jpa.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Social extends BaseEntity {
+    @ManyToOne
+    private Member member;
+
+    private String content;
+    private int like;
+
+    @OneToMany(mappedBy = "socialBoard", cascade = CascadeType.ALL)
+    private List<SocialShoes> socialShoes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "socialBoard", cascade = CascadeType.ALL)
+    private List<SocialComment> socialComments = new ArrayList<>();
 }
--- src/main/java/com/ll/hype/domain/social/social/entity/SocialShoes.java
+++ src/main/java/com/ll/hype/domain/social/social/entity/SocialShoes.java
@@ -1,4 +1,24 @@
 package com.ll.hype.domain.social.social.entity;
 
-public class SocialShoes {
+import com.ll.hype.domain.shoes.shoes.entity.Shoes;
+import com.ll.hype.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class SocialShoes extends BaseEntity {
+    @ManyToOne
+    private Social socialBoard;
+
+    @ManyToOne
+    private Shoes shoes;
 }
