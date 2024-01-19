e5ba9fad705cab9263336796b615a99a59482e4e
ChaChaMong <thflf93@gmail.com>
2024-01-19 09:39:52 +0900

    refactor: Status enum 완료 추가

--- src/main/java/com/ll/hype/domain/orderrequest/order/entity/Status.java
+++ src/main/java/com/ll/hype/domain/orderrequest/order/entity/Status.java
@@ -3,7 +3,8 @@ package com.ll.hype.domain.orderrequest.order.entity;
 public enum Status {
     BIDDING("BIDDING"),
     EXPIRED("EXPIRED"),
-    FAILED("FAILED");
+    FAILED("FAILED"),
+    COMPLETE("COMPLETE");
     private String value;
     Status(String value) {
 
