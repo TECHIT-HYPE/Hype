9bfdc321596fcfe83f3875a5f1f5e4649b60c2c5
eonjuseo <ejooss@chungbuk.ac.kr>
2024-01-23 22:47:22 +0900

    feat: ShoesRepository 작성

---
+++ src/main/java/com/ll/hype/domain/shoes/shoes/repository/ShoesRepository.java
@@ -0,0 +1,8 @@
+package com.ll.hype.domain.shoes.shoes.repository;
+
+import com.ll.hype.domain.shoes.shoes.entity.Shoes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoesRepository extends JpaRepository<Shoes, Long> {
+
+}
