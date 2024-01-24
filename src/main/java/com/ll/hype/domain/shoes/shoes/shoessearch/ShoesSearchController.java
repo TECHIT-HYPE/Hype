e34e80602bc1eeb093ad47f60d6baaae1f21d632
gayoi <117894300+gayoi@users.noreply.github.com>
2024-01-24 16:40:25 +0900

    feat: KorName search

--- src/main/java/com/ll/hype/domain/shoes/shoes/shoessearch/ShoesSearchController.java
+++ src/main/java/com/ll/hype/domain/shoes/shoes/shoessearch/ShoesSearchController.java
@@ -1,11 +1,15 @@
-package com.ll.hype.domain.shoes.shoes.shoesSearch;
+package com.ll.hype.domain.shoes.shoes.shoessearch;
 
+import com.ll.hype.domain.shoes.shoes.dto.ShoesResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import java.util.List;
 
 
 @Slf4j
@@ -13,18 +17,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/shoes")
 @Controller
 public class ShoesSearchController {
-    private final ShoesSearchService shoesSearchService;
+    private final com.ll.hype.domain.shoes.shoes.shoessearch.ShoesSearchService shoesSearchService;
 
     @GetMapping("/search")
     public String shesSearchForm() {
-        return "domain/shoes/searchForm";
+        return "domain/shoes/shoessearch/searchForm";
     }
 
     @PostMapping("/search")
     public String shoesSearch(@RequestParam(value = "keyword") String keyword,Model model) {
-        List<ShoesResponse> byKeyword = shoesSearchService.findByword(keyword);
-        model.addAttribute("shoes", byKeyword);
-        return "domain/shoes/list";
+        List<ShoesResponse> findByKeywordShoes = shoesSearchService.findByKeyword(keyword);
+        model.addAttribute("shoes", findByKeywordShoes);
+        return "domain/shoes/shoessearch/list";
     }
 
 }
--- src/main/java/com/ll/hype/domain/shoes/shoes/shoessearch/ShoesSearchService.java
+++ src/main/java/com/ll/hype/domain/shoes/shoes/shoessearch/ShoesSearchService.java
@@ -1,5 +1,6 @@
-package com.ll.hype.domain.shoes.shoes.shoesSearch;
+package com.ll.hype.domain.shoes.shoes.shoessearch;
 
+import com.ll.hype.domain.shoes.shoes.dto.ShoesResponse;
 import com.ll.hype.domain.shoes.shoes.entity.Shoes;
 import com.ll.hype.domain.shoes.shoes.repository.ShoesRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,8 @@ import java.util.List;
 public class ShoesSearchService {
     private final ShoesRepository shoesRepository;
 
+    // 해야하는것 :  모델명 , 브랜드(한국, 영어 2개있다)
+    // 신발 한국 이름 조회
     public List<ShoesResponse> findByKeyword(String keyword) {
         List<ShoesResponse> shoess = new ArrayList<>();
         List<Shoes> findByKorNames = shoesRepository.findByKorNameContainingIgnoreCase(keyword);
@@ -21,4 +24,16 @@ public class ShoesSearchService {
         }
         return shoess;
     }
+
+    // 신발 영어 이름 조회
+//    public List<ShoesResponse> findByEngword(String keyword) {
+//        List<ShoesResponse> shoess = new ArrayList<>();
+//        List<Shoes> findByEngNames = shoesRepository.findByEngNameContainingIgnoreCase(keyword);
+//        for (Shoes shoes : findByEngNames) {
+//            shoess.add(ShoesResponse.of(shoes));
+//        }
+//        return shoess;
+//    }
+
 }
+
