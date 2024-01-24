f4148037235476b9d90adf7a72a90c3274ec7dec
eonjuseo <ejooss@chungbuk.ac.kr>
2024-01-24 14:29:33 +0900

    style: ShoesController 어노테이션 순서 수정

--- src/main/java/com/ll/hype/domain/shoes/shoes/controller/ShoesController.java
+++ src/main/java/com/ll/hype/domain/shoes/shoes/controller/ShoesController.java
@@ -1,8 +1,6 @@
 package com.ll.hype.domain.shoes.shoes.controller;
 
-import com.ll.hype.domain.shoes.shoes.dto.ShoesRequest;
 import com.ll.hype.domain.shoes.shoes.dto.ShoesResponse;
-import com.ll.hype.domain.shoes.shoes.entity.Shoes;
 import com.ll.hype.domain.shoes.shoes.service.ShoesService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,23 +8,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
-@Controller
 @RequiredArgsConstructor
 @RequestMapping("/shoes")
+@Controller
 public class ShoesController {
 
     private final ShoesService shoesService;
 
     //신발 상세
     @GetMapping("/{id}")
-    public String shoesDetail(@PathVariable long id, Model model){
+    public String shoesDetail(@PathVariable("id") long id, Model model){
         ShoesResponse findOne = shoesService.findById(id);
         model.addAttribute("shoes", findOne);
         return "domain/shoes/shoes/detail";
