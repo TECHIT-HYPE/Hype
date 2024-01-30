b69afe665a9a976ade095601361540dc61a3637f
eonjuseo <ejooss@chungbuk.ac.kr>
2024-01-30 18:18:31 +0900

    feat: SaleController 구현

--- src/main/java/com/ll/hype/domain/order/sale/controller/SaleController.java
+++ src/main/java/com/ll/hype/domain/order/sale/controller/SaleController.java
@@ -4,13 +4,15 @@ import com.ll.hype.domain.order.buy.dto.BuyResponse;
 import com.ll.hype.domain.order.buy.service.BuyService;
 import com.ll.hype.domain.order.sale.dto.SaleRequest;
 import com.ll.hype.domain.order.sale.service.SaleService;
-import com.ll.hype.domain.shoes.shoes.dto.ShoesRequest;
 import com.ll.hype.domain.shoes.shoes.dto.ShoesResponse;
 import com.ll.hype.domain.shoes.shoes.dto.ShoesSizeDTO;
-import com.ll.hype.domain.shoes.shoes.entity.ShoesSize;
 import com.ll.hype.domain.shoes.shoes.service.ShoesService;
 import com.ll.hype.global.util.ShoesSizeGenerator;
+
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,69 +31,71 @@ public class SaleController {
     //사이즈 선택
     //신발고유번호, 이름, 이미지, 사이즈 노출
     @GetMapping("/select/{id}")
-    public String selectSizeForm(@PathVariable("id") Long id,
-                                 Model model,
-                                 SaleRequest saleRequest) {
+    public String selectSizeForm(@PathVariable("id") long id, Model model) {
         ShoesResponse shoesResponse = shoesService.findById(id);
         model.addAttribute("shoes", shoesResponse);
 
-        List<BuyResponse> orderRequestList = buyService.findByShoes(id);
-
+        Map<Integer, Optional<BuyResponse>> highestBidBySize = buyService.getHighestBidsGroupedBySize(id);
+        //사이즈가 존재하면 가격 노출, 존재하지않으면 판매 입찰
         List<ShoesSizeDTO> list = ShoesSizeGenerator.getSizes().stream()
-                .map(size -> {
-                    for (BuyResponse order : orderRequestList) {
-                        if (order.getShoesSize().getSize() == size) {
-                            return ShoesSizeDTO.builder()
-                                    .size(size)
-                                    .price(order.getPrice())
-                                    .exists(true)
-                                    .build();
-                        }
-                    }
-//++각 신발 사이즈별로 가장 높은 가격의 구매 입찰을 우선시하고, 가격이 같은 경우 먼저 입찰한 순서대로
-//++만료 기한
-                    return ShoesSizeDTO.builder()
-                            .exists(false)
-                            .size(size)
-                            .build();
-                })
+                .map(size -> highestBidBySize.getOrDefault(size, Optional.empty())
+                        .map(buyResponse -> ShoesSizeDTO.builder()
+                                .size(size)
+                                .price(buyResponse.getPrice())
+                                .exists(true)
+                                .build())
+                        .orElseGet(() -> ShoesSizeDTO.builder()
+                                .exists(false)
+                                .size(size)
+                                .build()))
                 .toList();
+
         model.addAttribute("shoesSizeList", list);
+
         return "domain/salesRequest/salesRequest/selectSize";
     }
 
     //사이즈 선택 처리
-    @PostMapping("/select/{id}")// 신발 id를 가져와야 하는데 오더리퀘스트아이디를
-    public String selectSize(@PathVariable("id") long id,
-                             @RequestParam(value = "selectedSize", required = true) int selectedSize,
-                             Model model) {
-        System.out.println("[SaleController.selectSize] selectedSize : " +  selectedSize);
-        BuyResponse buyResponse = buyService.findByShoesIdAndShoesSize(id, selectedSize);
-        model.addAttribute("buyResponse", buyResponse);
-        return "domain/salesRequest/salesRequest/approve";
-//        return "redirect:/sale/approval";
+    @PostMapping("/select/{id}")
+    public String selectSize(@PathVariable("id") Long id,
+                             @RequestParam("selectedSize") int selectedSize,
+                             RedirectAttributes redirectAttributes) {
+
+        Optional<BuyResponse> highestBid = buyService.getHighestBidsGroupedBySize(id).get(selectedSize);
+
+        highestBid.ifPresent(bid -> {
+            redirectAttributes.addFlashAttribute("selectedSize", selectedSize);
+            redirectAttributes.addFlashAttribute("highestBidId", bid.getId());
+        });
+
+        return "redirect:/sale/approval";
     }
 
     //판매 약관 동의
-//    @GetMapping("/approval")
-//    public String approveForm(
-//            @ModelAttribute("buyResponse") BuyResponse buyResponse,
-//            //@RequestParam("buyResponse") BuyResponse buyResponse,
-//                              Model model) {
-//        System.out.println("[SaleController.approveForm] buyResponse.getId() : " +  buyResponse.getId());
-//        System.out.println("[SaleController.approveForm] buyResponse.getShoesSize().getSize() : " +  buyResponse.getShoesSize());
-//        model.addAttribute("buyResponse", buyResponse);
-//        return "domain/salesRequest/salesRequest/approve";
-//    }
+    @GetMapping("/approval")
+    public String approveForm(@ModelAttribute("selectedSize") int selectedSize,
+                              @ModelAttribute("highestBidId") long highestBidId,
+                              Model model) {
+
+        BuyResponse buyResponse = buyService.findById(highestBidId);
+        model.addAttribute("buyResponse", buyResponse);
+
+        return "domain/salesRequest/salesRequest/approve";
+    }
 
     //판매 약관 동의 처리
     @PostMapping("/approval")
     public String approve() {
 
-        return "domain/salesRequest/salesRequest/pricing";
+        return "redirect:/sale/pricing";
     }
 
     //신발 정보 및 예상 정산 금액 표시, 판매 입찰 생성 기능
+    @GetMapping("/pricing")
+    public String pricing(BuyResponse buyResponse, Model model) {
+        model.addAttribute("buyResponse", buyResponse);
+        return "domain/salesRequest/salesRequest/pricing";
+    }
 
 
     //판매 결정 및 정보 입력
