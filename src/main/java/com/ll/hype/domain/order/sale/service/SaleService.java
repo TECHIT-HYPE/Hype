dde6a59ea6645993dbb03f870dae52139ab4fb04
eonjuseo <ejooss@chungbuk.ac.kr>
2024-01-31 15:48:36 +0900

    feat: SaleService 구현

--- src/main/java/com/ll/hype/domain/order/sale/service/SaleService.java
+++ src/main/java/com/ll/hype/domain/order/sale/service/SaleService.java
@@ -4,7 +4,9 @@ import com.ll.hype.domain.order.sale.dto.SaleRequest;
 import com.ll.hype.domain.order.sale.dto.SaleResponse;
 import com.ll.hype.domain.order.sale.entity.Sale;
 import com.ll.hype.domain.order.sale.repository.SaleRepository;
+import com.ll.hype.domain.shoes.shoes.dto.ShoesResponse;
 import com.ll.hype.domain.shoes.shoes.entity.ShoesSize;
+import com.ll.hype.domain.shoes.shoes.service.ShoesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,7 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class SaleService {
+    private final ShoesService shoesService;
     private final SaleRepository saleRepository;
+
+    public ShoesResponse findByShoesId(Long shoesId) {
+        return shoesService.findById(shoesId);
+    }
 
     public SaleResponse findById(long id) {
         Sale salesRequest = saleRepository.findById(id).get();
