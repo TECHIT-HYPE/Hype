ed899f8a60051bf873a9eda55024cf90fa96edac
tseop <77163842+seop-kim@users.noreply.github.com>
2024-02-02 05:45:06 +0900

    feat: Add ShoesSizeRepository query method

--- src/main/java/com/ll/hype/domain/shoes/shoes/repository/ShoesSizeRepository.java
+++ src/main/java/com/ll/hype/domain/shoes/shoes/repository/ShoesSizeRepository.java
@@ -1,7 +1,10 @@
 package com.ll.hype.domain.shoes.shoes.repository;
 
+import com.ll.hype.domain.shoes.shoes.entity.Shoes;
 import com.ll.hype.domain.shoes.shoes.entity.ShoesSize;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShoesSizeRepository extends JpaRepository<ShoesSize, Long> {
+    Optional<ShoesSize> findByShoesAndSize(Shoes shoes, int size);
 }
