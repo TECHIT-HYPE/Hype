d5e7ba34c6ef2b4b6f3499f5fc6aea68eea9f5d8
tseop <77163842+seop-kim@users.noreply.github.com>
2024-02-06 04:32:10 +0900

    comment: 주석 수정

--- src/main/java/com/ll/hype/domain/order/order/entity/Orders.java
+++ src/main/java/com/ll/hype/domain/order/order/entity/Orders.java
@@ -38,5 +38,5 @@ public class Orders extends BaseEntity {
     private Status status; // 거래 상태 (거래완료, 거래취소, 반품 진행 중, 반품완료, 거래중)
 
     @Enumerated(value = EnumType.STRING)
-    private SettlementStatus settlementStatus; // 정산상태 (결제완료(사용자), 결제취소(사용자), 미입금(관리자), 입금(관리자))
+    private SettlementStatus settlementStatus; // 정산상태 (결제대기(사용자) ,결제완료(사용자), 결제취소(사용자), 미입금(관리자), 입금(관리자))
 }
