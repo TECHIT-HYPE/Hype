b6d421333c3fc9ff8c6b18c4e52fca4757bf35a3
tseop <77163842+seop-kim@users.noreply.github.com>
2024-02-08 04:49:11 +0900

    refactor: Question method type 수정

--- src/main/java/com/ll/hype/domain/customer/question/controller/QuestionController.java
+++ src/main/java/com/ll/hype/domain/customer/question/controller/QuestionController.java
@@ -3,15 +3,19 @@ package com.ll.hype.domain.customer.question.controller;
 import com.ll.hype.domain.customer.question.dto.QuestionRequest;
 import com.ll.hype.domain.customer.question.dto.QuestionResponse;
 import com.ll.hype.domain.customer.question.service.QuestionService;
+import com.ll.hype.global.security.authentication.UserPrincipal;
 import java.security.Principal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,7 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/cs")
 @Controller
 public class QuestionController {
-    private final QuestionService csQService;
+    private final QuestionService questionService;
 
     @GetMapping("/main")
     public String questionMain() {
@@ -34,52 +38,51 @@ public class QuestionController {
     }
 
     @PostMapping("/question/create")
-    public String questionSave(QuestionRequest questionRequest,
-                               Principal principal,
-                               @RequestParam(value = "files") List<MultipartFile> files) {
-        String email = principal.getName();
-        csQService.questionSave(questionRequest, email, files);
+    public String questionSave(@AuthenticationPrincipal UserPrincipal user,
+                               @RequestParam(value = "files") List<MultipartFile> files,
+                               QuestionRequest questionRequest) {
+        questionService.questionSave(questionRequest, user.getMember(), files);
         return "redirect:/cs/main";
     }
 
     @GetMapping("/question/list")
-    public String questionList(Model model, Principal principal) {
-        String email = principal.getName();
-        List<QuestionResponse> findByQuestions = csQService.findByMyList(email);
+    public String questionList(@AuthenticationPrincipal UserPrincipal user,
+                               Model model) {
+        List<QuestionResponse> findByQuestions = questionService.findByMyList(user.getMember());
         model.addAttribute("questions", findByQuestions);
         return "domain/cs/question/list";
     }
+
     @GetMapping("/question/update/{id}")
     public String questionUpdateForm(@PathVariable("id") Long id,
-                                     Model model,
-                                     Principal principal,
-                                     QuestionRequest customerQRequest) {
-        String email = principal.getName();
-        QuestionResponse findQuestion = csQService.findOne(id, email);
+                                     @AuthenticationPrincipal UserPrincipal user,
+                                     QuestionRequest customerQRequest,
+                                     Model model) {
+        QuestionResponse findQuestion = questionService.findOne(id, user.getMember());
         model.addAttribute("question", findQuestion);
         return "domain/cs/question/update_form";
     }
 
-    @PostMapping("/question/update")
+    @PutMapping("/question/update")
     public String questionUpdate(@RequestParam("id") Long id,
-                                 QuestionRequest customerQRequest,
-                                 Principal principal) {
-        String email = principal.getName();
-        csQService.questionUpdate(id, customerQRequest,email);
+                                 @AuthenticationPrincipal UserPrincipal user,
+                                 QuestionRequest customerQRequest) {
+        questionService.questionUpdate(id, customerQRequest, user.getMember());
         return "redirect:/cs/question/detail/%s".formatted(id);
     }
 
-    @PostMapping("/question/delete/{id}")
-    public String questionDelete(@PathVariable("id") Long id, Principal principal) {
-        String email = principal.getName();
-        csQService.questionDelete(id, email);
+    @DeleteMapping("/question/delete/{id}")
+    public String questionDelete(@PathVariable("id") Long id,
+                                 @AuthenticationPrincipal UserPrincipal user) {
+        questionService.questionDelete(id, user.getMember());
         return "redirect:/cs/question/list";
     }
 
     @GetMapping("/question/detail/{id}")
-    public String questionDetail(@PathVariable("id") Long id, Model model, Principal principal) {
-        String email = principal.getName();
-        QuestionResponse findOne = csQService.findOne(id, email);
+    public String questionDetail(@PathVariable("id") Long id,
+                                 @AuthenticationPrincipal UserPrincipal user,
+                                 Model model) {
+        QuestionResponse findOne = questionService.findOne(id, user.getMember());
         model.addAttribute("question", findOne);
         return "domain/cs/question/detail";
     }
