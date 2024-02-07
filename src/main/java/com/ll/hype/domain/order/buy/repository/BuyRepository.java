872295a228fa1e30abff58341ad461b563e5804b
eonjuseo <ejooss@chungbuk.ac.kr>
2024-02-07 17:08:30 +0900

    feat: SaleController, SaleService, BuyRepository  판매가 결정, 판매 객체 저장 구현

--- src/main/java/com/ll/hype/domain/order/buy/repository/BuyRepository.java
+++ src/main/java/com/ll/hype/domain/order/buy/repository/BuyRepository.java
@@ -6,6 +6,7 @@ import com.ll.hype.domain.shoes.shoes.entity.Shoes;
 import com.ll.hype.domain.shoes.shoes.entity.ShoesSize;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -24,5 +25,13 @@ public interface BuyRepository extends JpaRepository<Buy, Long> {
                 "GROUP BY bb.shoesSize) " +
             "ORDER BY b.shoesSize.size ASC")
     List<Buy> findHighestPriceByShoesId(Shoes shoes);
+
+    @Query("SELECT b FROM Buy b WHERE b.shoes = :shoes " +
+            "AND b.status = 'BIDDING' " +
+            "AND b.shoesSize.size = :size " +
+            "AND b.price = (SELECT MAX(bb.price) FROM Buy bb " +
+            "WHERE bb.shoes = :shoes " +
+            "AND bb.shoesSize.size = :size)")
+    Buy findHighestPriceBuy(@Param("shoes") Shoes shoes, @Param("size") int size);
 
 }
