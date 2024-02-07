49069b2db99faf9bc4a9e38615c148fe1ad31be4
tseop <77163842+seop-kim@users.noreply.github.com>
2024-02-08 04:51:25 +0900

    feat: MyPage 내 구매내역 구현

--- src/main/java/com/ll/hype/domain/order/buy/repository/BuyRepository.java
+++ src/main/java/com/ll/hype/domain/order/buy/repository/BuyRepository.java
@@ -1,9 +1,11 @@
 package com.ll.hype.domain.order.buy.repository;
 
+import com.ll.hype.domain.member.member.entity.Member;
 import com.ll.hype.domain.order.buy.entity.Buy;
 import com.ll.hype.domain.order.sale.entity.Sale;
 import com.ll.hype.domain.shoes.shoes.entity.Shoes;
 import com.ll.hype.domain.shoes.shoes.entity.ShoesSize;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,10 +21,10 @@ public interface BuyRepository extends JpaRepository<Buy, Long> {
     @Query("SELECT b FROM Buy b WHERE b.shoes.id = :#{#shoes.id} " +
             "AND b.status = 'BIDDING' " +
             "AND (b.shoesSize, b.price) IN " +
-                "(SELECT bb.shoesSize, MAX(bb.price) FROM Buy bb " +
-                "WHERE bb.shoes.id = :#{#shoes.id} " +
-                "AND bb.status = 'BIDDING' " +
-                "GROUP BY bb.shoesSize) " +
+            "(SELECT bb.shoesSize, MAX(bb.price) FROM Buy bb " +
+            "WHERE bb.shoes.id = :#{#shoes.id} " +
+            "AND bb.status = 'BIDDING' " +
+            "GROUP BY bb.shoesSize) " +
             "ORDER BY b.shoesSize.size ASC")
     List<Buy> findHighestPriceByShoesId(Shoes shoes);
 
@@ -34,4 +36,5 @@ public interface BuyRepository extends JpaRepository<Buy, Long> {
             "AND bb.shoesSize.size = :size)")
     Buy findHighestPriceBuy(@Param("shoes") Shoes shoes, @Param("size") int size);
 
+    List<Buy> findByMember(Member member);
 }
