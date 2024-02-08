beab859b922595a3f03e809fc66326604b49a9e1
eonjuseo <ejooss@chungbuk.ac.kr>
2024-02-09 01:02:25 +0900

    feat: OrderService 수정

--- src/main/java/com/ll/hype/domain/order/order/service/OrderService.java
+++ src/main/java/com/ll/hype/domain/order/order/service/OrderService.java
@@ -1,19 +1,24 @@
 package com.ll.hype.domain.order.order.service;
 
 import com.ll.hype.domain.member.member.entity.Member;
+import com.ll.hype.domain.order.buy.dto.request.CreateBuyRequest;
 import com.ll.hype.domain.order.buy.entity.Buy;
 import com.ll.hype.domain.order.buy.repository.BuyRepository;
 import com.ll.hype.domain.order.order.dto.OrderRequest;
-import com.ll.hype.domain.order.order.dto.OrderResponse;
+import com.ll.hype.domain.order.order.dto.response.OrderResponse;
 import com.ll.hype.domain.order.order.entity.OrderStatus;
 import com.ll.hype.domain.order.order.entity.Orders;
 import com.ll.hype.domain.order.order.entity.PaymentStatus;
 import com.ll.hype.domain.order.order.repository.OrderRepository;
+import com.ll.hype.domain.order.sale.dto.request.CreateSaleRequest;
+import com.ll.hype.domain.order.sale.dto.response.SaleResponse;
 import com.ll.hype.domain.order.sale.entity.Sale;
 import com.ll.hype.domain.order.sale.repository.SaleRepository;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.ll.hype.domain.shoes.shoes.entity.Shoes;
+import com.ll.hype.domain.shoes.shoes.repository.ShoesRepository;
 import com.ll.hype.global.s3.image.ImageType;
 import com.ll.hype.global.s3.image.imagebridge.component.ImageBridgeComponent;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +33,8 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final BuyRepository buyRepository;
     private final SaleRepository saleRepository;
+    private final ShoesRepository shoesRepository;
+
     private final ImageBridgeComponent imageBridgeComponent;
 
     // 거래 체결
@@ -49,14 +56,23 @@ public class OrderService {
     // TODO
     // + 금액, 모델명, 사이즈가 다르면 거래불가
     // + !! buyResponse 주문 저장되면 status 변경
-    // + 운송장번호에 택배사 .. 택배사 통일?
 
-    public OrderResponse createOrder(OrderRequest orderRequest, long saleId, long buyId, Member member) {
-        Buy buy = buyRepository.findById(buyId) //orderRequest.getBuy().getId()
+    public OrderResponse createOrder(OrderRequest orderRequest, SaleResponse saleResponse, Member member) {
+        Shoes shoes = shoesRepository.findById(saleResponse.getShoes().getId())
+                .orElseThrow(() -> new IllegalArgumentException("조회된 신발이 없습니다."));
+
+        Buy buy = buyRepository.findHighestPriceBuy(shoes, saleResponse.getShoesSize().getSize()) //orderRequest.getBuy().getId()
                 .orElseThrow(() -> new IllegalArgumentException("조회된 구매 입찰이 없습니다."));
 
-        Sale sale = saleRepository.findById(saleId)
+        Sale sale = saleRepository.findById(saleResponse.getId())
                 .orElseThrow(() -> new IllegalArgumentException("조회된 판매 입찰이 없습니다."));
+
+        List<String> fullPath = imageBridgeComponent.findOneFullPath(ImageType.SHOES, saleResponse.getShoes().getId());
+
+//내가 판매할건데 이미 구매입찰 올라와있는 이 가격에 팔겠다 orderPrice=buy.getPrice
+//        if (!buy.getPrice().equals(sale.getPrice())) {
+//            throw new IllegalArgumentException("거래 성사 금액이 일치하지 않습니다.");
+//        }
 
         Orders order = Orders.builder()
                 .buy(buy)
@@ -71,7 +87,8 @@ public class OrderService {
                 .build();
         orderRepository.save(order);
 
-        return OrderResponse.of(order);
+//        order.updateTossId(order.createTossId());
+        return OrderResponse.of(order, fullPath);
     }
 
     public void checkAmount(String tossId, String amountStr) {
