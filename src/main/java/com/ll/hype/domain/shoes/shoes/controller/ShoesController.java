86942c02314e3a93d28d2dcef4a841e0cdbc6e47
tseop <77163842+seop-kim@users.noreply.github.com>
2024-02-10 07:10:12 +0900

    feat: 상품 찜 기능 관련 validate 기능 구현

--- src/main/java/com/ll/hype/domain/shoes/shoes/controller/ShoesController.java
+++ src/main/java/com/ll/hype/domain/shoes/shoes/controller/ShoesController.java
@@ -1,14 +1,24 @@
 package com.ll.hype.domain.shoes.shoes.controller;
 
+import com.ll.hype.domain.order.buy.dto.request.BuyConfirmRequest;
 import com.ll.hype.domain.shoes.shoes.dto.ShoesResponse;
+import com.ll.hype.domain.shoes.shoes.dto.ShoesWishCheckRequest;
 import com.ll.hype.domain.shoes.shoes.entity.ShoesCategory;
 import com.ll.hype.domain.shoes.shoes.service.ShoesService;
+import com.ll.hype.domain.wishlist.wishlist.service.WishlistService;
+import com.ll.hype.global.security.authentication.UserPrincipal;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,6 +32,7 @@ import java.util.stream.Collectors;
 @Controller
 public class ShoesController {
     private final ShoesService shoesService;
+    private final WishlistService wishlistService;
 
     // TODO
     // 1. 랭킹: 조회수 + 1 관심 + 10
@@ -46,5 +57,25 @@ public class ShoesController {
         model.addAttribute("selectedCategories", categories);
         model.addAttribute("shoesList", shoesList);
         return "domain/shoes/shoes/list";
+    }
+
+    @PostMapping("/love")
+    public String wish(@RequestParam("id") Long id,
+                       @RequestParam("selectSize") int selectSize,
+                       @AuthenticationPrincipal UserPrincipal user) {
+        wishlistService.addWishShoes(id, selectSize, user.getMember());
+        return "redirect:/shoes/%s".formatted(id);
+    }
+
+    @PostMapping("/love/check")
+    public ResponseEntity<String> checkWish(@RequestBody ShoesWishCheckRequest shoesWishCheckRequest,
+                                            @AuthenticationPrincipal UserPrincipal user) {
+
+        log.info("[ShoesController.checkWish] id : " + shoesWishCheckRequest.getShoesId());
+        log.info("[ShoesController.checkWish] size : " + shoesWishCheckRequest.getSize());
+        if (!wishlistService.checkShoesWish(shoesWishCheckRequest, user.getMember())) {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("이미 찜한 상품입니다..");
+        }
+        return ResponseEntity.ok().body("찜 가능");
     }
 }
