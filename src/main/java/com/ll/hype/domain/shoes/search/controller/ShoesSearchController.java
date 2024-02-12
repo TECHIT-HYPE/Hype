6b718647da0fa41f4afd187c92491a5f23b1aeda
tseop <77163842+seop-kim@users.noreply.github.com>
2024-02-13 01:49:43 +0900

    refactor: 검색 기능 mapping 수정

--- src/main/java/com/ll/hype/domain/shoes/search/controller/ShoesSearchController.java
+++ src/main/java/com/ll/hype/domain/shoes/search/controller/ShoesSearchController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,8 +25,9 @@ public class ShoesSearchController {
         return "domain/shoes/shoessearch/searchForm";
     }
 
-    @PostMapping("/search")
-    public String shoesSearch(@RequestParam(value = "keyword") String keyword, Model model) {
+    @GetMapping("/search/keyword")
+    public String shoesSearch(@RequestParam(value = "keyword") String keyword,
+                              Model model) {
         ShoesSearchResponse data = shoesSearchService.findByKeyword(keyword);
         model.addAttribute("data", data);
         return "domain/shoes/shoessearch/list";
