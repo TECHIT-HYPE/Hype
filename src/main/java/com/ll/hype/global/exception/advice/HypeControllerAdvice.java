d8f81e7d1cc9a6d9b9dcf0537c2f456d243d3ead
tseop <77163842+seop-kim@users.noreply.github.com>
2024-02-13 01:52:31 +0900

    refactor: controller advice handler 추가

--- src/main/java/com/ll/hype/global/exception/advice/HypeControllerAdvice.java
+++ src/main/java/com/ll/hype/global/exception/advice/HypeControllerAdvice.java
@@ -1,12 +1,26 @@
 package com.ll.hype.global.exception.advice;
 
+import com.ll.hype.global.exception.custom.EntityNotFoundException;
+import com.ll.hype.global.exception.dto.ErrorResponse;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class HypeControllerAdvice {
-    //@ExceptionHandler(IllegalArgumentException.class)
-    //public String exceptionMain(IllegalArgumentException e) {
-        //return "redirect:/";
-    //}
+    private static final String ERROR_PAGE = "global/error/error_page";
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String exceptionMain(IllegalArgumentException e, Model model) {
+        ErrorResponse errorResponse = ErrorResponse.of(404L, e);
+        model.addAttribute("data", errorResponse);
+        return ERROR_PAGE;
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public String entityNotFoundException(EntityNotFoundException e, Model model) {
+        ErrorResponse errorResponse = ErrorResponse.of(404L, e);
+        model.addAttribute("data", errorResponse);
+        return ERROR_PAGE;
+    }
 }
